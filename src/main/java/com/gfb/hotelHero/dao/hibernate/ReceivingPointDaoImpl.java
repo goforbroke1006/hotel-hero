922722de18cec39a059bc33a4f39d1d11c7a8583
Sergey <go.for.broke1006@gmail.com>
2018-01-12 01:17:39 +0300

    Update ReceivingPointDaoImpl.java

--- src/main/java/com/gfb/hotelHero/dao/hibernate/ReceivingPointDaoImpl.java
+++ src/main/java/com/gfb/hotelHero/dao/hibernate/ReceivingPointDaoImpl.java
@@ -2,12 +2,17 @@ package com.gfb.hotelHero.dao.hibernate;
 
 import com.gfb.hotelHero.dao.ReceivingPointDao;
 import com.gfb.hotelHero.domain.ReceivingPoint;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ReceivingPointDaoImpl
         extends BaseDao<ReceivingPoint, Long>
         implements ReceivingPointDao {
+    
+    @Autowired
+    private SessionFactory sessionFactory;
 
     @Override
     public void delete(Long id) {

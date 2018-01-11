b08aa009f1ef583383aab59aa9707c170be2af7e
Sergey <go.for.broke1006@gmail.com>
2018-01-12 01:14:17 +0300

    Update HibernateUtil.java

--- src/main/java/com/gfb/hotelHero/dao/hibernate/util/HibernateUtil.java
+++ src/main/java/com/gfb/hotelHero/dao/hibernate/util/HibernateUtil.java
@@ -3,8 +3,6 @@ package com.gfb.hotelHero.dao.hibernate.util;
 import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-//import org.hibernate.service.ServiceRegistry;
-//import org.hibernate.service.ServiceRegistryBuilder;
 
 import javax.validation.Validation;
 import javax.validation.Validator;
@@ -16,29 +14,9 @@ public class HibernateUtil {
 
     private static SessionFactory sessionFactory;
 
-    /*static {
-        try {
-            sessionFactory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            LOGGER.error(ex.getMessage());
-            throw new ExceptionInInitializerError(ex);
-        }
-    }*/
-
     public static SessionFactory getSessionFactory() {
         if (null == sessionFactory) {
-//            Configuration configuration = new Configuration();
-//            configuration.configure();
-//
-//            Properties properties = configuration.getProperties();
-//
-//            ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(properties).buildServiceRegistry();
-//            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-
             sessionFactory = new Configuration().configure().buildSessionFactory();
-
-//            ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-//            Validator validator = factory.getValidator();
         }
         return sessionFactory;
     }

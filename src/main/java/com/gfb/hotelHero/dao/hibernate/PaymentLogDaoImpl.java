0464cd59a7c623e893768ba643a66bed9bcebd13
goforbroke <go.for.broke1006@gmail.com>
2018-01-13 21:20:57 +0300

    Remove unused code

--- src/main/java/com/gfb/hotelHero/dao/hibernate/PaymentLogDaoImpl.java
+++ src/main/java/com/gfb/hotelHero/dao/hibernate/PaymentLogDaoImpl.java
@@ -1,7 +1,6 @@
 package com.gfb.hotelHero.dao.hibernate;
 
 import com.gfb.hotelHero.dao.PaymentLogDao;
-import com.gfb.hotelHero.dao.hibernate.util.HibernateUtil;
 import com.gfb.hotelHero.domain.PaymentLog;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
@@ -10,12 +9,5 @@ import java.util.List;
 
 @Repository
 public class PaymentLogDaoImpl extends BaseDao<PaymentLog, Long> implements PaymentLogDao {
-
-//    public List<PaymentLog> findAll() {
-//        Session session = HibernateUtil.getSessionFactory().openSession();
-//        return session
-//                .createQuery("SELECT _pl FROM com.gfb.hotelHero.domain.PaymentLog _pl")
-//                .list();
-//    }
 
 }
